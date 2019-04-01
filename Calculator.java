ed05d2b18d7fe7224c6fd5f5f6b6bc0bd13cf7e7
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-03-31 20:24:41 -0400

    added isOperation which checks if the thing is an opertaion

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,13 @@ public class Calculator{
    *Assume valid postfix notation, separated by spaces.
    */
   public static double eval(String s){
+    MyDeque<String> queue = new MyDeque<String>();
     double ans = 0;
     return ans;
+
+  }
+
+  public static boolean isOperation(String s){
+    return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/") || s.equals("%");
   }
 }
